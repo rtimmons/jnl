453499f8e66624724f42210c9ec2a9d901891071
Ryan Timmons <rtimmons@amazon.com>
2015-12-19 10:37:44 -0500

    initial impl of worklog.pl and jnl library

---
+++ bin/worklog.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl -w
+use strict;
+use warnings;
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+use Carp::Heavy;
+
+use jnl qw(dbdir guid open_file);
+
+sub journal_file_name {
+    my ($basedir, $guid) = @_;
+    return "$basedir/$guid.txt";
+}
+
+sub write_file {
+    my ($guid, $jfile) = @_;
+    open  JFILE, ">$jfile" or die "Couldn't write $jfile: $!";
+    print JFILE ("\n" x 4) . "My Reference: $guid  \n";
+    close JFILE;
+}
+
+sub main {
+    my $guid = guid 20;
+    my $jfile = journal_file_name(dbdir(), $guid);
+    write_file($guid, $jfile);
+    open_file($jfile);
+}
+
+
+if ( $0 eq __FILE__ ) {
+    main();
+}
---
+++ lib/jnl.pm
@@ -0,0 +1,35 @@
+package jnl;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(dbdir guid open_file);
+
+sub dbdir {
+    $ENV{WORKLOGS} || "$ENV{HOME}/Projects/WorkLogs";
+}
+
+sub guid {
+    my $length = shift || 20;
+    my @elts = qw(
+        0 1 2 3 4 5 6 7 8 9 A B C D E F G H J K M N P Q R S T U W X Y Z
+    );
+    my $pid = $$;
+    
+    my @out = ();
+    for( 0 .. $length ) {
+        # 13 is an arbitrary prime number
+        # 
+        # xor in the pid since two instances running at same time
+        # might get the same result from rand()
+        my $rand = rand( scalar(@elts) * 13 ^ $pid ) % scalar(@elts);
+        $out[$_] = $elts[ $rand ];
+    }
+    join '', @out;
+}
+
+sub open_file {
+    my ($file) = @_;
+    system qq{open -a TextMate "$file"};
+}
+
+1
