f87a9988b80a2cf2e5fc565dca7017cf5a1cd25c
Ryan Timmons <ryan@rytim.com>
2017-11-28 17:18:13 -0500

    duplicate fixtures

--- tests/test_jnl.py
+++ tests/test_jnl.py
@@ -1,11 +1,39 @@
 import sys
 import os
+import shutil
+import tempfile
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'bin'))
+bin_dir = os.path.join(os.path.dirname(__file__), '..', 'bin')
+sys.path.insert(0, bin_dir)
+
 import jnl
 
 import unittest
 import mock
 
-class DatabaseTest(unittest.TestCase):
-    pass
+fixture_dir = os.path.join(bin_dir, '..', 'tests', 'fixtures')
+
+class TestDatabase(unittest.TestCase):
+
+    def main_with_fixture(self, fixture='typical'):
+        fixture_path = os.path.join(fixture_dir, fixture)
+        tmpdir = tempfile.mkdtemp()
+        subdir = os.path.join(tmpdir, fixture)
+        print "Copy %s -> %s" % (fixture_path, subdir)
+        shutil.copytree(fixture_path, subdir)
+        self.to_cleanup.append(tmpdir)
+        return jnl.Main({'JNL_DIR': subdir}), tmpdir
+
+    def setUp(self):
+        self.to_cleanup = []
+        pass
+
+    def tearDown(self):
+        for c in self.to_cleanup:
+            print "Cleaning up %s" % c
+            shutil.rmtree(c)
+
+    def test_create_entry(self):
+        main, nl = self.main_with_fixture('typical')
+        print main.context.database.path()
+
