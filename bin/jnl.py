ab359151e8d83593b99bf5662c1f9db9b52407f8
Ryan Timmons <ryan.timmons@mongodb.com>
2019-10-09 10:37:28 -0400

    wip

--- bin/jnl.py
+++ bin/jnl.py
@@ -64,7 +64,7 @@ class Database(object):
     def entry_with_guid(self, guid: str) -> Entry:
         return [e for e in self.entries if e.guid == guid][0]
 
-    def entries_with_tag(self, name: str, value: str) -> List[Entry]:
+    def entries_with_tag(self, name: str, value: str = None) -> List[Entry]:
         return [e for e in self.entries if e.has_tag(name, value)]
 
     def daily_entry(self, yyyymmdd: str = None) -> Entry:
@@ -79,6 +79,13 @@ class Database(object):
                 )
             ]
         return existing[0]
+
+    def yesterday_entry(self, yyyymmdd_today: str = None) -> Entry:
+        if yyyymmdd_today is None:
+            yyyymmdd_today = self.context.what_day_is_it.yyyymmdd()
+        existing = [
+            e for e in self.entries_with_tag("quick") if e.is_a_daily_entry()
+        ].sort(key=lambda e: print(e))
 
     def scan(self) -> None:
         # TODO: multi-thread all of this nonsense
@@ -124,6 +131,15 @@ class Tag(object):
         re.VERBOSE,
     )
 
+    DAILY_RE = re.compile(
+        r"""
+        ^
+        daily/(.*?)     # group 1: date
+        $
+        """,
+        re.VERBOSE,
+    )
+
     @staticmethod
     def parse(line: str) -> List[Tag]:
         """Return list of tags"""
@@ -142,6 +158,14 @@ class Tag(object):
             value = re_match.group(2)
         self._name: str = name
         self._value: str = value
+
+    def daily(self) -> Optional[str]:
+        if self.name != "quick" or not self.value:
+            return None
+        match = Tag.DAILY_RE.match(self.value)
+        if match:
+            return match.group(1)
+        return None
 
     @property
     def name(self) -> str:
@@ -210,6 +234,15 @@ class Entry(object):
 
         if create:
             self._create()
+
+    def is_a_daily_entry(self) -> Optional[str]:
+        """
+        :return: if this entry has @quick(daily/X) returns X else None
+        """
+        tags = [t for t in self.tags if t.daily()]
+        if not tags:
+            return None
+        return tags[0].daily()
 
     def file_path(self) -> str:
         return os.path.join(self.path, self.file_name)
@@ -438,7 +471,7 @@ class System(object):
         return os.path.isdir(path)
 
     @staticmethod
-    def now():
+    def now() -> datetime:
         return datetime.datetime.now()
 
 
@@ -696,6 +729,12 @@ class Main(object):
         raise ValueError("Don't know about action {}".format(argv[1]))
 
     def scan(self, _):
+        self.context.database.scan()
+
+    # TODO: finish
+    def yesterday(self, _):
+        daily = self.context.database.yesterday_entry()
+        self.context.opener.open(daily)
         self.context.database.scan()
 
     def daily(self, _):
