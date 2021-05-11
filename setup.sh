83665cbc679f2501952c4e9e029dcf6b8a628fde
Ryan Timmons <ryan.timmons@mongodb.com>
2021-05-11 11:48:55 -0400

    make setup more robust to pyenv already being installed

--- setup.sh
+++ setup.sh
@@ -4,10 +4,18 @@ set -eo pipefail
 
 if [[ ! -d "venv" || ! -e "venv/setup-done" ]]; then
     rm -rf venv
-    brew install pyenv
+    if ! command -v pyenv; then
+        if command -v brew; then
+            brew install pyenv
+        else
+            echo "Don't know how to install pyenv without homebrew." >&2
+        fi
+    fi
+
     pyenv install -s
     pyenv rehash
     python3 -mvenv venv
+    # shellcheck disable=SC1091
     source ./venv/bin/activate
     python3 -m pip install --upgrade pip setuptools wheel
     pip install -r requirements.txt
